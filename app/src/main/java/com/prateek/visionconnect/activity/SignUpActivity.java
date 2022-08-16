306daa1f5226d345e3e5b75d1e1dea7c714eb62a
Prateek0001 <prateekofficial0001@gmail.com>
2022-08-16 23:35:16 +0530

    Designed SignUp page

--- app/src/main/java/com/prateek/visionconnect/activity/SignUpActivity.java
+++ app/src/main/java/com/prateek/visionconnect/activity/SignUpActivity.java
@@ -2,28 +2,26 @@ package com.prateek.visionconnect.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.prateek.visionconnect.R;
-import com.prateek.visionconnect.databinding.ActivityLoginBinding;
+import com.prateek.visionconnect.databinding.ActivitySignUpBinding;
 
-public class LoginActivity extends AppCompatActivity {
-
-    ActivityLoginBinding binding;
+public class SignUpActivity extends AppCompatActivity {
+    ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.tvRegister.setOnClickListener(new View.OnClickListener() {
+        binding.tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
