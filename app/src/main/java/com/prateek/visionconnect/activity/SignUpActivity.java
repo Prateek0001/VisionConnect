83c322d74916c161d2cc0568531b5863fad27aba
Prateek0001 <prateekofficial0001@gmail.com>
2022-08-17 23:16:14 +0530

    Firebase Authentication SignUp

--- app/src/main/java/com/prateek/visionconnect/activity/SignUpActivity.java
+++ app/src/main/java/com/prateek/visionconnect/activity/SignUpActivity.java
@@ -1,22 +1,77 @@
 package com.prateek.visionconnect.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-import com.prateek.visionconnect.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.prateek.visionconnect.MainActivity;
 import com.prateek.visionconnect.databinding.ActivitySignUpBinding;
+import com.prateek.visionconnect.model.UserModel;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    String TAG = "SignUpActivity";
     ActivitySignUpBinding binding;
+    private FirebaseAuth mAuth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mAuth.createUserWithEmailAndPassword(binding.emailET.getText().toString(), binding.passwordET.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    UserModel userModel = new UserModel(
+                                            binding.nameET.getText().toString(),
+                                            binding.professionET.getText().toString(),
+                                            binding.emailET.getText().toString(),
+                                            binding.passwordET.getText().toString());
+
+                                    String id = task.getResult().getUser().getUid();
+                                    database.getReference().child("Users").child(id).setValue(userModel);
+                                    Toast.makeText(SignUpActivity.this, "User Data saved", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+
+                                    Log.d(TAG, "createUserWithEmail:success");
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
 
         binding.tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
