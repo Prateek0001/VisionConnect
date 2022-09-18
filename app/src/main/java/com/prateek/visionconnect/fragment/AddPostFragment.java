0ce7b067e639d867a9d59fa000085d74a3fbcb67
Prateek0001 <prateekofficial0001@gmail.com>
2022-09-18 23:27:43 +0530

    Choose image to post

--- app/src/main/java/com/prateek/visionconnect/fragment/AddPostFragment.java
+++ app/src/main/java/com/prateek/visionconnect/fragment/AddPostFragment.java
@@ -1,17 +1,24 @@
 package com.prateek.visionconnect.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
+import com.prateek.visionconnect.R;
 import com.prateek.visionconnect.databinding.FragmentAddPostBinding;
 
-public class AddPostFragment extends Fragment {
+public class AddPostFragment extends BaseFragment {
 
     FragmentAddPostBinding binding;
+    Uri uri;
 
     public AddPostFragment() {
         // Required empty public constructor
@@ -26,9 +33,61 @@ public class AddPostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         binding = FragmentAddPostBinding.inflate(inflater, container, false);
 
+        binding.etPostDescription.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String description = binding.etPostDescription.getText().toString();
+                if (!description.isEmpty()) {
+                    binding.btnPost.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_btn_bg));
+                    binding.btnPost.setTextColor(getContext().getResources().getColor(R.color.white));
+                    binding.btnPost.setEnabled(true);
+                } else {
+                    binding.btnPost.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_active_btn));
+                    binding.btnPost.setTextColor(getContext().getResources().getColor(R.color.dark_grey));
+                    binding.btnPost.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        binding.addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data.getData() != null) {
+
+            uri = data.getData();
+            binding.postImage.setImageURI(uri);
+            binding.postImage.setVisibility(View.VISIBLE);
+            //enable post button
+            binding.btnPost.setBackgroundDrawable(ContextCompat.getDrawable(getContext(), R.drawable.follow_btn_bg));
+            binding.btnPost.setTextColor(getContext().getResources().getColor(R.color.white));
+            binding.btnPost.setEnabled(true);
+
+        }
     }
 }
